64cd90d3fb1aff0e2ba7cee6229c403251331761
Akiko Yamasaki <paonpaon@gmail.com>
2023-05-15 11:40:55 +1000

    components

---
+++ src/testcomponents/Frametwo.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Header from "./header";
+import Sidebar from "./Sidebar";
+const Frametwo= () => {
+    return(
+        <>
+            <div className="frame-two">
+                <Header />
+                <div className=""></div>
+                <Sidebar />
+            </div>
+        </>
+    );
+    };
+
+export default Frametwo
